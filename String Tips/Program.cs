789ec1029106f7127e6a3fd0e190aa0ea8eeb95b
Mohammed Alaa <74833312+cohelper@users.noreply.github.com>
2021-12-31 21:22:00 +0200

    sec lession

---
+++ String Tips/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/*@ is called verbatim Literal we use it to translate skip sequnce as normal printable charcters*/
+namespace String_Tips
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string quotes = "str1";
+            string NewlineWithskipSeq = "before\nAfter";
+            string toPrintSlah = "\\one Slah";
+            string toIgnoreSkipSeq = @"\\two slash ..skip seq is ignored";
+            Console.WriteLine(quotes);
+            Console.WriteLine();
+            Console.WriteLine(NewlineWithskipSeq);
+            Console.WriteLine();
+            Console.WriteLine(toPrintSlah);
+            Console.WriteLine();
+            Console.WriteLine(toIgnoreSkipSeq);
+
+        }
+    }
+}
