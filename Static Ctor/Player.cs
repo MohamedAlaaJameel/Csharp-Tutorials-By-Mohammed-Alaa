c4c075e3186222bbf943bd3dd9f14f3d924f49b4
Mohammed Alaa <74833312+cohelper@users.noreply.github.com>
2022-01-02 06:10:00 +0200

    static ctor

---
+++ Static Ctor/Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Ctor
+{
+    class Player
+    {
+        static int PlayersNum;
+        public static int bla;
+        static Player()
+        {
+            Console.WriteLine("player static ctor called once");
+            PlayersNum = 0;
+        }
+        public Player()
+        {
+            PlayersNum++;
+            Console.WriteLine($" player num {PlayersNum} has been created ");
+        }
+    }
+}
---
+++ Static Ctor/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//we use it to init static data .
+//called one time only . after using any class static var (any static var)
+//you can say static ctor is called on class first usage 
+// you can't add paramters to static ctor 
+//static ctor is called once no matter how many instances u create 
+//its called before any other ctor 
+
+
+namespace Static_Ctor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player.bla = 1; //static ctor is called 
+            Console.WriteLine("after calling static ctor");
+            List<Player> players = new List<Player>();
+            for (int i = 0; i < 10; i++)
+            {
+                players.Add(new Player());
+            }
+
+        }
+    }
+}
