ca2afc7624a0f27d211477d1ef796cc6941127a5
Mohammed Alaa <74833312+cohelper@users.noreply.github.com>
2022-01-02 06:22:49 +0200

    Hiding methods

---
+++ Method Hiding/MyPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Hiding
+{
+    class MyPlayer:Player
+    {
+        // new keyword to hide the method and avoid the warning of method hiding . 
+        //if new is not written the method will be hidden and np , only warning will pop up.
+        public new void Run()
+        {
+            Console.WriteLine("My player is rurning");
+
+          //  base.Run();//calling base method
+        }
+    }
+}
---
+++ Method Hiding/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Hiding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyPlayer myPlayer = new MyPlayer();
+            Console.WriteLine("type cast to parent object to call the parent method");
+            ((Player)myPlayer).Run();
+            Console.WriteLine("Calling the hidden method  run  on Myplayer ");
+            myPlayer.Run();
+        }
+    }
+}
