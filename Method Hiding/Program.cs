1dcb5ae1e355a5f5c0f9b85601341a64eaa6bc2e
Mohammed Alaa <74833312+cohelper@users.noreply.github.com>
2022-01-02 06:33:05 +0200

    .

--- Method Hiding/Program.cs
+++ Method Hiding/Program.cs
@@ -12,9 +12,24 @@ namespace Method_Hiding
         {
             MyPlayer myPlayer = new MyPlayer();
             Console.WriteLine("type cast to parent object to call the parent method");
+
+            //cast to parent object 
             ((Player)myPlayer).Run();
             Console.WriteLine("Calling the hidden method  run  on Myplayer ");
             myPlayer.Run();
+
+
+            //parent point on child data .
+            Player player = new MyPlayer();//polymorphism ..base point to dervied class
+            player.Run();//run will be called on base .
+
+
+            /*
+             to call parent method you need to : 
+            -cast to parent Or point to child 
+             
+             
+             */
         }
     }
 }
